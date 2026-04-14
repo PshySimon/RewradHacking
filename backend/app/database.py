288e25e0538b0ad156a9934da5929a1ba21b2acc
PhsySimon <caixiaomeng2@huawei.com>
2026-04-14 22:14:28 +0800

    fix: 扩大 SQLite 数据库连接池以解决多图片粘贴导致 fetch API 连接池耗尽 500 错误

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,10 +7,18 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(os.path.dirname(BASE_DIR), "hacking.db")
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
+from sqlalchemy import create_engine, pool
+
 # 创建数据库引擎
 # check_same_thread 设定是为了适配 SQLite 本地多线程通信
+# 扩大 pool_size 和 max_overflow 应对前端由于“一键粘贴多图”导致的疯狂并发请求从而引发的 DB 线程池枯竭并 500 的问题
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False},
+    poolclass=pool.QueuePool,
+    pool_size=100,
+    max_overflow=150,
+    pool_timeout=60
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
